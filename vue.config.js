2c30dfbfa1a69448f16bd3ab07b901ec34ee9fa7
lihaoyu <250970849@qq.com>
2022-03-28 23:40:36 +0800

    修改文件路径

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+/*
+ * @Author: lihaoyu
+ * @Date: 2022-03-28 22:16:15
+ * @LastEditTime: 2022-03-28 23:38:18
+ * @LastEditors: lihaoyu
+ * @Description:
+ * @FilePath: /sherly-vue3/vue.config.js
+ */
+const { defineConfig } = require("@vue/cli-service");
+module.exports = defineConfig({
+  transpileDependencies: true,
+});
