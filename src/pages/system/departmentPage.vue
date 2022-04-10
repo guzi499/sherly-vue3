9d8e59466cf45fe2de90880700f46153dc2fbb0b
shijingjing <1789544664@qq.com>
2022-04-10 17:35:22 +0800

    部门  权限

---
+++ src/pages/system/departmentPage.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="departmentPage">
+    部门管理
+  </div>
+</template>
+
+<script setup>
+// 部门管理
+</script>
+
+<style></style>
---
+++ src/pages/system/permissionPage.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="permissionPage">
+    权限管理
+  </div>
+</template>
+
+<script setup>
+// 权限管理
+</script>
+
+<style lang="scss" scope>
+.permissionPage {
+}
+</style>
