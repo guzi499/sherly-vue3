cedd5e663becef5c7f837b281ce2a64b490ae8de
陈雪丽 <cxl193832236>
2022-04-16 13:56:46 +0800

    菜单页面搭建完成

---
+++ src/api/system/menu.js
@@ -0,0 +1,27 @@
+import request from "@/utils/request";
+
+/**新增菜单 */
+export function addMenu(data) {
+  return request({
+    url: "/menu/save/one",
+    method: "post",
+    data: data,
+  });
+}
+
+// 修改菜单
+export function updateMenu(data) {
+  return request({
+    url: "/menu/update/one",
+    method: "put",
+    data: data,
+  });
+}
+
+// 删除菜单
+export function delMenu(menuId) {
+  return request({
+    url: "/menu/update/one" + menuId,
+    method: "delete",
+  });
+}
--- src/pages/system/menuPage.vue
+++ src/pages/system/menuPage.vue
@@ -21,23 +21,20 @@
           >新增</el-button
         >
       </el-col>
-      <!-- <el-col :span="1.5">
-        <el-button type="danger" plain size="small" @click="handleDelete('1')"
-          >批量删除</el-button
-        >
-      </el-col> -->
     </el-row>
     <!-- 表格菜单 -->
     <el-table
-      :data="tableData"
+      :data="menuList"
       style="width: 100%; margin-bottom: 20px"
-      row-key="id"
+      row-key="menuId"
       border
-      default-expand-all
+      lazy
+      :tree-props="{ children: 'children', hasChildren: 'hasChildren' }"
     >
       <el-table-column
         label="菜单名称"
         prop="menuName"
+        sortable
         width="230"
         align="center"
       />
@@ -68,273 +65,56 @@
       </el-table-column>
     </el-table>
     <!-- 新增 / 编辑 弹框 -->
-    <el-dialog
-      v-model="dialogEditFormVisible"
-      :title="title"
-      class="editDialog"
-    >
-      <el-form :model="editForm" label-width="120px">
-        <el-form-item label="上级菜单">
-          <el-select
-            v-model="editForm.parentId"
-            clearable
-            placeholder="Select"
-            style="width: 90%"
-          >
-            <el-option
-              ref="option"
-              class="option"
-              :value="optionData.id"
-              :label="optionData.name"
-            >
-              <el-tree
-                :data="menuList"
-                :props="defaultProps"
-                @node-click="handleNodeClick"
-              ></el-tree>
-            </el-option>
-          </el-select>
-        </el-form-item>
-        <el-form-item label="菜单类型">
-          <el-radio-group v-model="editForm.type">
-            <el-radio label="目录" />
-            <el-radio label="菜单" />
-            <el-radio label="按钮" />
-          </el-radio-group>
-        </el-form-item>
-        <el-form-item label="菜单图标">
-          <el-select
-            style="width: 90%"
-            v-model="editForm.icon"
-            multiple
-            filterable
-            remote
-            reserve-keyword
-            placeholder="点击选择图标"
-            :remote-method="remoteMethod"
-          >
-            <el-option
-              v-for="item in icons"
-              :key="item.value"
-              :label="item.label"
-              :value="item.value"
-            />
-          </el-select>
-        </el-form-item>
-        <el-form-item label="菜单名称">
-          <el-select
-            style="width: 90%"
-            v-model="editForm.icon"
-            multiple
-            filterable
-            remote
-            reserve-keyword
-            placeholder="请输入菜单名称"
-            :remote-method="remoteMethod"
-          >
-            <el-option
-              v-for="item in icons"
-              :key="item.value"
-              :label="item.label"
-              :value="item.value"
-            />
-          </el-select>
-        </el-form-item>
-        <el-form-item label="显示排序">
-          <el-input-number
-            v-model="editForm.sort"
-            :min="1"
-            :max="10"
-            controls-position="right"
-            size="large"
-            @change="handleChangeSort"
-          />
-        </el-form-item>
-        <el-form-item label="是否外链">
-          <el-radio
-            v-model="editForm.radio1"
-            label="是"
-            value="1"
-            size="large"
-          />
-          <el-radio
-            v-model="editForm.radio1"
-            label="否"
-            value="0"
-            size="large"
-          />
-        </el-form-item>
-        <el-form-item label="路由地址">
-          <el-select
-            style="width: 90%"
-            v-model="editForm.icon"
-            multiple
-            filterable
-            remote
-            reserve-keyword
-            placeholder="请输入菜单名称"
-            :remote-method="remoteMethod"
-          >
-            <el-option
-              v-for="item in icons"
-              :key="item.value"
-              :label="item.label"
-              :value="item.value"
-            />
-          </el-select>
-        </el-form-item>
-        <el-form-item label="显示状态">
-          <el-radio
-            v-model="editForm.show1"
-            label="显示"
-            value="1"
-            size="large"
-          />
-          <el-radio
-            v-model="editForm.show1"
-            label="隐藏"
-            value="0"
-            size="large"
-          />
-        </el-form-item>
-        <el-form-item label="菜单状态">
-          <el-radio
-            v-model="editForm.state1"
-            label="正常"
-            value="1"
-            size="large"
-          />
-          <el-radio
-            v-model="editForm.state1"
-            label="停用"
-            value="0"
-            size="large"
-          />
-        </el-form-item>
-      </el-form>
-      <template #footer>
-        <span class="dialog-footer">
-          <el-button @click="dialogEditFormVisible = false">取消</el-button>
-          <el-button type="primary" @click="dialogEditFormVisible = false"
-            >确定</el-button
-          >
-        </span>
-      </template>
-    </el-dialog>
   </div>
 </template>
 
 <script>
-import { reactive, ref } from "vue";
 import { useStore } from "vuex";
+import { ElMessage, ElMessageBox } from "element-plus";
 export default {
   setup() {
     const $store = useStore();
     // 获取路由信息
     const menuList = $store.state.router.menuList;
-    console.log(menuList);
-    // 表格数据
-    const tableData = reactive([
-      {
-        menuId: 0,
-        menuName: "",
-        link: "/",
-        icon: "/",
-        sort: 0,
-        currentDate: '2022-4-21 12:12:12',
-        // parentId: 0,
-      },
-    ]);
-    // 弹框标题
-    const title = ref("标题");
-    // 新增/编辑 弹框是否可见
-    const dialogEditFormVisible = ref(false);
-    // 新增/编辑 弹框绑定的表格数据
-    const editForm = reactive({
-      parentId: "主类目",
-      type: "",
-      icon: "",
-      sort: Number,
-      radio1: "1",
-      show1: "1",
-      state1: "1",
-    });
-    // 上级菜单
-    const defaultProps = reactive({
-      children: "children",
-      label: "menuName",
-    });
-    const optionData = reactive({
-      id: "",
-      name: "",
-    });
-    // 菜单图标
-    const icons = reactive([]);
-
-    // 编辑按钮
+    // 点击修改 - 新增按钮
     const handleEdit = (type, index, data) => {
-      console.log("index = ", index, "data", data);
-      dialogEditFormVisible.value = true;
-      // 新增
       if (type === "1") {
-        console.log(title);
-        // title.value = '新增菜单'
-        console.log("点击新增");
+        console.log("新增 === ", type, index, data);
       }
-      // 编辑 / 修改
       if (type === "2") {
-        console.log(title);
-        // title.value = '编辑菜单'
-        console.log("点击编辑");
+        console.log("修改 === ", type, index, data);
       }
+      console.log();
     };
-
-    // 删除按钮
+    // 点击删除按钮
     const handleDelete = (type, index, data) => {
-      console.log("index = ", index, "data", data);
-      // 批量删除
-      if (type === "1") {
-        console.log("点击批量删除");
-      }
-      // 删除
       if (type === "2") {
-        console.log("点击删除");
+        console.log("删除 ===", type, index, data);
+        ElMessageBox.confirm(
+          "确定删除本条数据？",
+          // "Warning",
+          {
+            confirmButtonText: "确定",
+            cancelButtonText: "取消",
+            type: "warning",
+          }
+        )
+          .then(() => {
+            console.log(data.menuId);
+            ElMessage({
+              type: "success",
+              message: "删除成功",
+            });
+          })
+          .catch(() => {
+            ElMessage({
+              type: "info",
+              message: "取消删除操作",
+            });
+          });
       }
     };
-
-    // 上级菜单
-    const handleNodeClick = (data) => {
-      if (data) {
-        console.log(data);
-        // router.push({ path: data.link });
-      }
-    };
-
-    // 新增 / 编辑 远程搜索事件
-    const remoteMethod = () => {
-      console.log("远程搜索");
-    };
-
-    // 新增 / 编辑 排序框change事件
-    const handleChangeSort = (val) => {
-      console.log(val);
-    };
-
-    return {
-      tableData,
-      handleEdit,
-      handleDelete,
-      dialogEditFormVisible,
-      editForm,
-      menuList,
-      title,
-      defaultProps,
-      optionData,
-      handleNodeClick,
-      remoteMethod,
-      icons,
-      handleChangeSort,
-    };
+    return { menuList, handleEdit, handleDelete };
   },
 };
 </script>
