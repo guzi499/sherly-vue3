af88cce21eb8c5f10a3520703944d8e2033f408a
sherly <1193832236@qq.com>
2022-06-16 08:52:48 +0800

    用户管理 - 添加表单校验

--- src/pages/system/userPage.vue
+++ src/pages/system/userPage.vue
@@ -130,14 +130,14 @@
     />
     <!--    新增 / 修改弹框-->
     <el-dialog v-model="dialogFormVisible" :title="dialogTitle" width="40%">
-      <el-form :model="form" style="padding-right: 60px">
+      <el-form :model="form" :rules="formRules" ref="ruleForm" style="padding-right: 60px">
         <el-form-item label="手机号" :label-width="formLabelWidth" prop="phone">
           <el-input v-model="form.phone" :disabled="type1 === '2'"></el-input>
         </el-form-item>
         <el-form-item
-          label="姓名"
-          :label-width="formLabelWidth"
-          prop="realName"
+            label="姓名"
+            :label-width="formLabelWidth"
+            prop="realName"
         >
           <el-input v-model="form.realName"></el-input>
         </el-form-item>
@@ -156,7 +156,7 @@
             <el-radio :label="0" size="large">女</el-radio>
           </el-radio-group>
         </el-form-item>
-        <el-form-item label="部门" :label-width="formLabelWidth">
+        <el-form-item label="部门" :label-width="formLabelWidth" prop="departmentId">
           <el-select
               ref="selectTree1"
               v-model="form.departmentId"
@@ -199,7 +199,7 @@
         </el-form-item>
         <el-form-item :label-width="formLabelWidth">
           <el-button @click="handleCancle">取消</el-button>
-          <el-button type="primary" @click="handleOk">确定</el-button>
+          <el-button type="primary" @click="handleOk('ruleForm')">确定</el-button>
         </el-form-item>
       </el-form>
     </el-dialog>
@@ -370,44 +370,97 @@ export default {
       proxy.$refs.selectTree1.blur();
     };
 
+    // 自定义校验手机号
+    // const validatePass = (rule, value, callback) => {
+    //   const reg = /^(13[0-9]|14[01456879]|15[0-35-9]|16[2567]|17[0-8]|18[0-9]|19[0-35-9])\d{8}$/g
+    //   if (!value) {
+    //     return callback(new Error('手机号不能为空'));
+    //   } else {
+    //     if (!Number(value)) {
+    //       callback(new Error('请输入数字值'));
+    //     }
+    //     else {
+    //       console.log(!reg.test(value))
+    //       if (!reg.test(value)) {
+    //         console.log(1111111)
+    //         callback(new Error('不合法手机号!!!'));
+    //       } else {
+    //         console.log(22222222)
+    //         callback();
+    //       }
+    //     }
+    //   }
+    // }
+
+    // 表格添加校验效果
+    const formRules = {
+      // phone: [
+      //   {validator: validatePass, trigger: 'blur'}
+      // ],
+      phone: [
+        { required: true, message: '请输入手机号码', trigger: 'blur' },
+        { min: 11, max: 11, message: '长度在 11 个字符', trigger: 'blur' }
+      ],
+      realName: [
+        {required: true, message: '请输入姓名', trigger: 'blur'}
+      ],
+      gender: [
+        {required: true, message: '请选择性别', trigger: 'change'}
+      ],
+      departmentId: [
+        {required: true, message: '请选择部门', trigger: 'change'}
+      ],
+      roleIds: [
+        {required: true, message: '请选择角色', trigger: 'change'}
+      ]
+    }
+
     // 确定按钮
-    const handleOk = () => {
-      if (type1.value === "1") {
-        delete form.value.departmentName;
-        addUser(form.value)
-            .then(() => {
-              ElMessage({
-                message: "新增成功！",
-                type: "success",
-              });
-              data.queryParams.current = 1
-              getList();
-              dialogFormVisible.value = false;
-            })
-          .catch(() => {
-            ElMessage.error("新增失败");
-          });
-      } else {
-        delete form.value.departmentName;
-        delete form.value.createTime;
-        delete form.value.enable;
-        delete form.value.email;
-        delete form.value.nickname;
-        delete form.value.phone;
-        updataUser(form.value)
-            .then(() => {
-              ElMessage({
-                message: "修改成功！",
-                type: "success",
-              });
-              data.queryParams.current = 1
-              getList();
-              dialogFormVisible.value = false;
-            })
-          .catch(() => {
-            ElMessage.error("修改失败");
-          });
-      }
+    const handleOk = (formName) => {
+      proxy.$refs[formName].validate((valid) => {
+        if (valid) {
+          if (type1.value === "1") {
+            delete form.value.departmentName;
+            addUser(form.value)
+                .then(() => {
+                  ElMessage({
+                    message: "新增成功！",
+                    type: "success",
+                  });
+                  data.queryParams.current = 1
+                  getList();
+                  dialogFormVisible.value = false;
+                })
+                .catch(() => {
+                  ElMessage.error("新增失败");
+                });
+          } else {
+            delete form.value.departmentName;
+            delete form.value.createTime;
+            delete form.value.enable;
+            delete form.value.email;
+            delete form.value.nickname;
+            delete form.value.phone;
+            updataUser(form.value)
+                .then(() => {
+                  ElMessage({
+                    message: "修改成功！",
+                    type: "success",
+                  });
+                  data.queryParams.current = 1
+                  getList();
+                  dialogFormVisible.value = false;
+                })
+                .catch(() => {
+                  ElMessage.error("修改失败");
+                });
+          }
+        } else {
+          console.log('error submit!!');
+          return false;
+        }
+      });
+
     };
 
     // 取消按钮
@@ -448,6 +501,7 @@ export default {
     };
 
     return {
+      formRules,
       type1,
       ...toRefs(data),
       total,
