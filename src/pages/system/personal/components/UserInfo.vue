af411b7c4cea725ea327426cedfb7b15c0956fe6
陈雪丽 <2441756138@qq.com>
2022-07-02 20:23:27 +0800

    个人中心修改

--- src/pages/system/personal/components/UserInfo.vue
+++ src/pages/system/personal/components/UserInfo.vue
@@ -1,33 +1,69 @@
 <template>
   <div class="box-userInfo">
     <el-form :model="curUserInfo" style="margin-bottom: 24px">
-      <el-form-item label="昵称：">
+      <el-form-item>
+        <strong slot="label" class="icon">
+          <i class="iconfont icon-nicheng"></i>
+           昵称：
+        </strong>
         <span>{{ curUserInfo.nickname }}</span>
       </el-form-item>
-      <el-form-item label="姓名：">
+      <el-form-item>
+        <strong slot="label" class="icon">
+          <i class="iconfont icon-zuoxixingming"></i>
+           姓名：
+        </strong>
         <span>{{ curUserInfo.realName }}</span>
       </el-form-item>
-      <el-form-item label="性别：">
+      <el-form-item>
+        <strong slot="label" class="icon">
+          <i class="iconfont icon-xingbie"></i>
+           性别：
+        </strong>
         <span>{{ curUserInfo.genderStr }}</span>
       </el-form-item>
-      <el-form-item label="手机号：">
+      <el-form-item>
+        <strong slot="label" class="icon">
+          <i class="iconfont icon-shoujihao"></i>
+           手机号：
+        </strong>
         <span>{{ curUserInfo.phone }}</span>
       </el-form-item>
-      <el-form-item label="邮箱：">
+      <el-form-item>
+        <strong slot="label" class="icon">
+          <i class="iconfont icon-youxiang"></i>
+           邮箱：
+        </strong>
         <span>{{ curUserInfo.email }}</span>
       </el-form-item>
-      <el-form-item label="租户：">
+      <el-form-item>
+        <strong slot="label" class="icon">
+          <i class="iconfont icon-jiaoseguanlizuhu"></i>
+           租户：
+        </strong>
         <span>{{ curUserInfo.tenantName }}</span>
       </el-form-item>
-      <el-form-item label="部门：">
+      <el-form-item>
+        <strong slot="label" class="icon">
+          <i class="iconfont icon-bumen"></i>
+           部门：
+        </strong>
         <span>{{ curUserInfo.departmentName }}</span>
       </el-form-item>
-      <el-form-item label="角色：">
+      <el-form-item>
+        <strong slot="label" class="icon">
+          <i class="iconfont icon-jiaose"></i>
+           角色：
+        </strong>
         <span>
           <text v-for="(item, index) in curUserInfo.roleNames">{{ item + '、 ' }}</text>
         </span>
       </el-form-item>
-      <el-form-item label="创建时间：">
+      <el-form-item>
+        <strong slot="label" class="icon">
+          <i class="iconfont icon-nicheng"></i>
+           创建时间：
+        </strong>
         <span>{{ curUserInfo.createTime }}</span>
       </el-form-item>
     </el-form>
@@ -65,8 +101,21 @@ export default {
   border-bottom: 1px solid #efefef;
 }
 
-span {
-  width: 100%;
-  text-align: right;
+::v-deep .el-form-item__content {
+  display: flex;
+  justify-content: space-between;
+  .icon {
+    width: 30%;
+    display: flex;
+    align-items: center;
+    i{
+      margin-right: 4px;
+    }
+  }
+  span {
+    width: 70%;
+  }
 }
+
+
 </style>
