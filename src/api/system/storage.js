041afdbfa783d22ebf41ccb1e36c3015bab8f109
lihaoyu <250970849@qq.com>
2022-07-14 00:58:58 +0800

    feat:feat:对象存储模块

--- src/api/system/storage.js
+++ src/api/system/storage.js
@@ -1,7 +1,7 @@
 /*
  * @Author: lihaoyu
  * @Date: 2022-07-08 23:23:03
- * @LastEditTime: 2022-07-09 23:15:51
+ * @LastEditTime: 2022-07-14 00:51:30
  * @LastEditors: lihaoyu
  * @Description:
  * @FilePath: /sherly-vue3/src/api/system/storage.js
@@ -11,7 +11,7 @@ import request from "@/utils/request";
 /**获取对象存储列表 */
 export function getStorageList(params) {
   return request({
-    url: "/oss/config/list/page",
+    url: "/oss_config/list_page",
     method: "get",
     params,
   });
@@ -20,7 +20,7 @@ export function getStorageList(params) {
 /**保存对象存储 */
 export function saveStorage(data) {
   return request({
-    url: "/oss/config/save/one",
+    url: "/oss_config/save_one",
     method: "post",
     data,
   });
@@ -28,7 +28,29 @@ export function saveStorage(data) {
 
 export function deleteStorage(configId) {
   return request({
-    url: "oss/config/remove/one?configId=" + configId,
+    url: "oss_config/remove_one?configId=" + configId,
     method: "DELETE",
+  });
+}
+
+export function getStorageDetail(configId) {
+  return request({
+    url: "oss_config/get_one?configId=" + configId,
+    method: "GET",
+  });
+}
+
+export function upDataStorage(data) {
+  return request({
+    url: "oss_config/update_one",
+    method: "put",
+    data,
+  });
+}
+
+export function enableStorage(configId) {
+  return request({
+    url: "oss_config/enable_one?configId=" + configId,
+    method: "put",
   });
 }
--- src/pages/system/storage/storageDialog.vue
+++ src/pages/system/storage/storageDialog.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: lihaoyu
  * @Date: 2022-07-08 23:40:21
- * @LastEditTime: 2022-07-09 23:39:42
+ * @LastEditTime: 2022-07-13 23:58:35
  * @LastEditors: lihaoyu
  * @Description: 
  * @FilePath: /sherly-vue3/src/pages/system/storage/storageDialog.vue
@@ -22,7 +22,6 @@
     >
       <el-form-item
         label="配置名称："
-        required
         :label-width="formLabelWidth"
         prop="configName"
       >
@@ -37,22 +36,29 @@
       </el-form-item>
       <el-form-item
         label="存储方式："
-        required
         :label-width="formLabelWidth"
         prop="type"
       >
-        <el-select v-model="form.type" size="default">
+        <el-select v-model="form.type" size="default" clearable>
           <el-option
             v-for="item in typeOptions"
             :key="item.value"
             :label="item.label"
             :value="item.value"
+            :disabled="item.disabled"
           />
         </el-select>
       </el-form-item>
       <el-form-item
+        v-if="form.type === 5"
+        label="上传地址："
+        :label-width="formLabelWidth"
+        prop="config.endpoint"
+      >
+        <el-input v-model="form.config.endpoint" />
+      </el-form-item>
+      <el-form-item
         label="存储桶："
-        required
         :label-width="formLabelWidth"
         prop="config.bucket"
       >
@@ -60,12 +66,34 @@
       </el-form-item>
       <el-form-item
         label="访问域名："
-        required
         :label-width="formLabelWidth"
         prop="config.domainName"
       >
         <el-input v-model="form.config.domainName" />
       </el-form-item>
+      <block v-if="form.type === 5">
+        <el-form-item
+          label="访问Key："
+          :label-width="formLabelWidth"
+          prop="config.accessKey"
+        >
+          <el-input v-model="form.config.accessKey" />
+        </el-form-item>
+        <el-form-item
+          label="访问Secret："
+          :label-width="formLabelWidth"
+          prop="config.accessSecret"
+        >
+          <el-input v-model="form.config.accessSecret" />
+        </el-form-item>
+        <el-form-item
+          label="访问地区："
+          :label-width="formLabelWidth"
+          prop="config.region"
+        >
+          <el-input v-model="form.config.region" />
+        </el-form-item>
+      </block>
     </el-form>
     <template #footer>
       <span class="dialog-footer">
@@ -77,8 +105,8 @@
 </template>
 
 <script>
-import { reactive, ref } from "vue";
-import { saveStorage } from "@/api/system/storage";
+import { reactive, ref, watch } from "vue";
+import { saveStorage, upDataStorage } from "@/api/system/storage";
 import { ElMessage } from "element-plus";
 
 export default {
@@ -98,45 +126,114 @@ export default {
     const resetForm = () => {
       Object.assign(form, initForm());
     };
-    const formLabelWidth = "100px";
+    const formLabelWidth = "110px";
     const typeOptions = [
-      { label: "数据库", value: 1 },
+      { label: "数据库", value: 1, disabled: true },
       { label: "本地存储", value: 2 },
-      { label: "FTP", value: 3 },
-      { label: "SFTP", value: 4 },
+      { label: "FTP", value: 3, disabled: true },
+      { label: "SFTP", value: 4, disabled: true },
       { label: "S3", value: 5 },
     ];
     const formRef = ref(null);
-    const rules = {
-      configName: [
-        {
-          required: true,
-          message: "请输入配置名称",
-          trigger: "blur",
-        },
-      ],
-      type: [
-        {
-          required: true,
-          message: "请选择存储类型",
-          trigger: "blur",
-        },
-      ],
-      "config.bucket": [
-        {
-          required: true,
-          message: "请输入存储桶",
-          trigger: "blur",
-        },
-      ],
-      "config.domainName": [
-        {
-          required: true,
-          message: "请输入访问域名",
-          trigger: "blur",
-        },
-      ],
-    };
+    const rules = ref({});
+
+    watch(
+      form,
+      (newVal) => {
+        if (newVal.type !== 5) {
+          rules.value = {
+            configName: [
+              {
+                required: true,
+                message: "请输入配置名称",
+                trigger: "change",
+              },
+            ],
+            type: [
+              {
+                required: true,
+                message: "请选择存储类型",
+                trigger: "change",
+              },
+            ],
+            "config.bucket": [
+              {
+                required: true,
+                message: "请输入存储桶",
+                trigger: "change",
+              },
+            ],
+            "config.domainName": [
+              {
+                required: true,
+                message: "请输入访问域名",
+                trigger: "change",
+              },
+            ],
+          };
+        } else if (newVal.type === 5) {
+          rules.value = {
+            configName: [
+              {
+                required: true,
+                message: "请输入配置名称",
+                trigger: "change",
+              },
+            ],
+            type: [
+              {
+                required: true,
+                message: "请选择存储类型",
+                trigger: "change",
+              },
+            ],
+            "config.bucket": [
+              {
+                required: true,
+                message: "请输入存储桶",
+                trigger: "change",
+              },
+            ],
+            "config.domainName": [
+              {
+                required: true,
+                message: "请输入访问域名",
+                trigger: "change",
+              },
+            ],
+            "config.endpoint": [
+              {
+                required: true,
+                message: "请输入上传地址",
+                trigger: "change",
+              },
+            ],
+            "config.accessKey": [
+              {
+                required: true,
+                message: "请输入访问Key",
+                trigger: "change",
+              },
+            ],
+            "config.accessSecret": [
+              {
+                required: true,
+                message: "请输入访问Secret",
+                trigger: "change",
+              },
+            ],
+            "config.region": [
+              {
+                required: true,
+                message: "请输入访问地区",
+                trigger: "change",
+              },
+            ],
+          };
+        }
+      },
+      { immediate: true, deep: true }
+    );
     const handleCancel = () => {
       dialogTableVisible.value = false;
       resetForm();
@@ -148,16 +245,34 @@ export default {
           if (form.type === 2) {
             form.config["@class"] =
               "com.guzi.upr.storage.client.local.LocalOssClientConfig";
+            delete form.config.endpoint;
+            delete form.config.accessKey;
+            delete form.config.accessSecret;
+            delete form.config.region;
+          } else if (form.type === 5) {
+            form.config["@class"] =
+              "com.guzi.upr.storage.client.s3.S3OssClientConfig";
           }
-          saveStorage(form).then(() => {
-            ElMessage({
-              message: "添加成功！",
-              type: "success",
+          if (isEdit.value) {
+            upDataStorage(form).then(() => {
+              ElMessage({
+                message: "修改成功！",
+                type: "success",
+              });
+              context.emit("updata");
+              dialogTableVisible.value = false;
             });
-            context.emit("updata");
-            dialogTableVisible.value = false;
-            resetForm();
-          });
+          } else {
+            saveStorage(form).then(() => {
+              ElMessage({
+                message: "添加成功！",
+                type: "success",
+              });
+              context.emit("updata");
+              dialogTableVisible.value = false;
+            });
+          }
+          resetForm();
         }
       });
     };
