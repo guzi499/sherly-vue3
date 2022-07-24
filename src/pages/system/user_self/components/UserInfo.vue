d08867b7336c8e6531ee1a1e70421b1c25fff2f1
陈雪丽 <2441756138@qq.com>
2022-07-24 17:32:37 +0800

    个人中心 - 编辑用户添加校验效果

--- src/pages/system/user_self/components/UserInfo.vue
+++ src/pages/system/user_self/components/UserInfo.vue
@@ -79,23 +79,23 @@
     </el-row>
     <!--添加编辑用户弹框-->
     <el-dialog v-model="EditDialogVisible" title="信息编辑" width="25%" center>
-      <el-form :model="formEdit" label-width="60px">
+      <el-form :model="formEdit" label-width="60px"  ref="formEditRef" :rules="formEditRules">
         <el-form-item label="手机号">
           <el-input v-model="formEdit.phone" disabled />
         </el-form-item>
-        <el-form-item label="昵称">
+        <el-form-item label="昵称" prop="nickname">
           <el-input v-model="formEdit.nickname" />
         </el-form-item>
         <el-form-item label="姓名">
           <el-input v-model="formEdit.realName" disabled />
         </el-form-item>
-        <el-form-item label="性别">
+        <el-form-item label="性别" prop="gender">
           <el-radio-group v-model="formEdit.gender">
             <el-radio :label="1">男</el-radio>
             <el-radio :label="0">女</el-radio>
           </el-radio-group>
         </el-form-item>
-        <el-form-item label="邮箱">
+        <el-form-item label="邮箱" prop="email">
           <el-input v-model="formEdit.email" />
         </el-form-item>
         <el-form-item label="部门">
@@ -108,7 +108,7 @@
       <template #footer>
         <span class="dialog-footer">
           <el-button @click="EditDialogVisible = false">取消</el-button>
-          <el-button type="primary" @click="handleOk">确定</el-button>
+          <el-button type="primary" @click="handleOk('formEditRef')">确定</el-button>
         </span>
       </template>
     </el-dialog>
@@ -178,29 +178,55 @@ export default {
       reset();
       formEdit.value = JSON.parse(JSON.stringify(userInfo));
       EditDialogVisible.value = true;
+      setTimeout(() => {
+        proxy.$refs['formEditRef'].resetFields();
+      }, 50)
     };
     // 编辑用户 - 确定按钮
-    const handleOk = () => {
-      // console.log(formEdit.value)
-      updateSelf(formEdit.value).then(() => {
-        ElMessage({
-          showClose: true,
-          message: "更新用户信息成功",
-          type: "success",
-        });
-      }).finally(() => {
-        // 初始化数据
-        emit('getList')
-      });
-      EditDialogVisible.value = false
-
+    const handleOk = (formName) => {
+      proxy.$refs[formName].validate((valid) => {
+        if(valid){
+          updateSelf(formEdit.value).then(() => {
+            ElMessage({
+              showClose: true,
+              message: "更新用户信息成功",
+              type: "success",
+            });
+            // 初始化数据
+            emit('getList')
+          }).finally(() => {
+            EditDialogVisible.value = false
+          })
+        } else {
+          return false;
+        }
+      })
     }
 
     // 添加修改密码弹框是否可见
     const PwdDialogVisible = ref(false)
     // 添加修改密码弹框 - 表单数据
     const formPwd = ref()
-    // 密码表单校验
+
+    /* 编辑用户表单校验 */
+    const formEditRules = reactive({
+      nickname: [
+        {required: true, message: '用户昵称不能为空', trigger: 'blur'},
+        { min: 3, max: 7, message: '用户昵称长度在 3 到 7 个字符', trigger: 'blur' }
+      ],
+      gender: [
+        {required: true, message: '用户性别不能为空', trigger: 'change'}
+      ],
+      email: [
+        {required: true, message: '用户邮箱不能为空', trigger: 'blur'},
+        {
+          pattern: '^\\w+([-+.]\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*$',
+          message: '邮箱格式错误！'
+        }
+      ],
+    })
+
+    /* 密码表单校验 */
     const rules = reactive({
       oldPassword: [
         {required: true, message: '旧密码不能为空', trigger: 'blur'}
@@ -212,6 +238,7 @@ export default {
         {required: true, message: '确认密码不能为空', trigger: 'blur'}
       ],
     })
+
     // 重置修改密码表单
     const resetPwd = () => {
       formPwd.value = {}
@@ -270,6 +297,7 @@ export default {
       PwdDialogVisible,
       formEdit,
       formPwd,
+      formEditRules,
       rules,
       handleEdit,
       handlePwd,
