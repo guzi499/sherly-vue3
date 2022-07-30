656d9c4f118081af6c7341f0cb522888f5e25a48
陈雪丽 <2441756138@qq.com>
2022-07-30 14:59:17 +0800

    个人中心 - 操作日志改造

--- src/pages/system/user_self/components/OperationLog.vue
+++ src/pages/system/user_self/components/OperationLog.vue
@@ -11,75 +11,29 @@
         :pagination-current="tableData.current"
         :pagination-size="tableData.size"
     >
-      <template #header>
-        <el-popconfirm
-            confirm-button-text="是"
-            cancel-button-text="否"
-            :icon="InfoFilled"
-            icon-color="#626AEF"
-            title="确认清空所有日志?"
-            @confirm="handleEmpty"
-        >
-          <template #reference>
-            <el-button type="danger" size="small"> 清空日志</el-button>
-          </template>
-        </el-popconfirm>
-      </template>
       <template #table>
         <el-table-column prop="logId" label="日志id" width="100" align="center"/>
-        <el-table-column prop="type" label="日志类型" width="180" align="center" show-overflow-tooltip>
+        <el-table-column prop="type" label="日志类型" width="80" align="center" show-overflow-tooltip>
           <template #default="scope">
             <span>{{ scope.row.type === 1 ? '异常' : '正常' }}</span>
           </template>
         </el-table-column>
         <el-table-column prop="description" label="描述" align="center" show-overflow-tooltip/>
-        <el-table-column prop="requestMethod" label="请求方式" align="center"/>
-        <el-table-column prop="uri" label="请求uri" align="center" show-overflow-tooltip width="180px"/>
-        <el-table-column prop="ip" label="请求ip" align="center" show-overflow-tooltip/>
-        <el-table-column prop="os" label="请求设备" align="center" show-overflow-tooltip width="180px"/>
+        <el-table-column prop="os" label="请求设备" align="center" show-overflow-tooltip/>
         <el-table-column prop="address" label="请求地址" align="center" show-overflow-tooltip/>
         <el-table-column prop="browser" label="请求浏览器" align="center" show-overflow-tooltip/>
         <el-table-column prop="duration" label="耗时" align="center"/>
         <el-table-column prop="createTime" label="操作时间" align="center" width="180"/>
-        <el-table-column prop="createTime" label="操作" align="center" width="100" fixed="right">
-          <template #default="scope">
-            <a class="link_style" @click="handleLogId(scope.row.logId)">详情</a>
-          </template>
-        </el-table-column>
       </template>
     </SherlyTable>
-    <!--查看日志详情-->
-    <el-dialog
-        v-model="dialogVisible"
-        title="日志详情"
-        width="30%"
-        center
-    >
-      <el-descriptions class="margin-top" :column="1">
-        <el-descriptions-item label="日志id:">{{ formInfo.logId }}</el-descriptions-item>
-        <el-descriptions-item label="日志类型:">
-          <el-tag size="small">{{ formInfo.type === 1 ? '异常' : '正常' }}</el-tag>
-        </el-descriptions-item>
-        <el-descriptions-item label="描述:">{{ formInfo.description }}</el-descriptions-item>
-        <el-descriptions-item label="请求方式:">{{ formInfo.requestMethod }}</el-descriptions-item>
-        <el-descriptions-item label="请求uri:">{{ formInfo.uri }}</el-descriptions-item>
-        <el-descriptions-item label="请求参数:">{{ formInfo.requestParams }}</el-descriptions-item>
-        <el-descriptions-item label="请求ip:">{{ formInfo.ip }}</el-descriptions-item>
-        <el-descriptions-item label="请求地址:">{{ formInfo.address }}</el-descriptions-item>
-        <el-descriptions-item label="请求浏览器:">{{ formInfo.browser }}</el-descriptions-item>
-        <el-descriptions-item label="耗时:">{{ formInfo.duration }}</el-descriptions-item>
-        <el-descriptions-item label="异常详情:">{{ formInfo.exception }}</el-descriptions-item>
-      </el-descriptions>
-    </el-dialog>
   </div>
 </template>
 
 <script>
 import {reactive, ref, onMounted} from "vue";
 import {useRoute} from "vue-router";
-import {getOperationList, delLog, getOperationOne} from '@/api/system/operate.js'
+import {getOperationList} from '@/api/system/operate.js'
 import SherlyTable from "@/components/SherlyTable";
-import {ElMessage} from "element-plus";
 import {InfoFilled} from '@element-plus/icons-vue'
 
 export default {
@@ -93,9 +47,6 @@ export default {
       size: 10,
       userId: parseInt(route.query.userId)
     })
-    /* 查看日志详情数据 */
-    const dialogVisible = ref(false)
-    const formInfo = ref({})
 
     onMounted(() => {
       getList()
@@ -110,26 +61,6 @@ export default {
           loading.value = false
         }, 100)
       });
-    }
-
-    /* 清空日志 */
-    const handleEmpty = () => {
-      formInfo.value = {}
-      delLog().then(() => {
-        ElMessage({
-          message: "清除成功",
-          type: "success",
-        });
-        queryParams.current = 1
-        getList()
-      })
-    }
-
-    /* 查看日志详情 */
-    const handleLogId = async (logId) => {
-      const data = await getOperationOne(logId)
-      formInfo.value = data
-      dialogVisible.value = true
     }
 
     /* 修改当前分页页码 */
@@ -151,11 +82,7 @@ export default {
       tableData,
       handleCurrentChange,
       handleSizeChange,
-      handleEmpty,
       InfoFilled,
-      handleLogId,
-      dialogVisible,
-      formInfo
     }
   }
 }
