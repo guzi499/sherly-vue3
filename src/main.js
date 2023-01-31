18c77f875bc25b8f0a7fb3177020e2ea6276fce9
谷子毅 <504305797@qq.com>
2023-01-31 18:03:26 +0800

    add pinia

--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './router/router.ts'
+import store from './store/index.js'
 
 const app = createApp(App)
 app.use(router)
+app.use(store)
 app.mount('#app')
