e4d12ecfbd57d48d1383e997d12d81a5abd08cec
谷子毅 <504305797@qq.com>
2023-01-31 23:09:52 +0800

    rename main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-import router from './router/router.ts'
-import store from './store/index.js'
+import router from './router/router'
+import store from './store/index'
 
 const app = createApp(App)
 app.use(router)
