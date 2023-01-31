bd23e95662ab0ff12d42943b93e7422dbc6eb897
谷子毅 <504305797@qq.com>
2023-01-31 17:57:58 +0800

    add axios

---
+++ src/api/http.ts
@@ -0,0 +1,29 @@
+import axios, {AxiosInstance} from "axios";
+
+const axiosInstance: AxiosInstance = axios.create({
+  // 前缀
+  baseURL: "",
+  // 超时
+  timeout: 1000 * 30,
+  // 请求头
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+const service = {
+  get<T = any>(url: string, data?: object): Promise<T> {
+    return axiosInstance.get(url, data);
+  },
+  post<T = any>(url: string, data?: object): Promise<T> {
+    return axiosInstance.post(url, data);
+  },
+  put<T = any>(url: string, data?: object): Promise<T> {
+    return axiosInstance.put(url, data);
+  },
+  delete<T = any>(url: string, data?: object): Promise<T> {
+    return axiosInstance.delete(url, data);
+  }
+}
+
+export default service;
