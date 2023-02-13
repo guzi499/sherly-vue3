25889c8a76df572bdc17acb6f8ced2970cbde476
陈雪丽 <2441756138@qq.com>
2023-02-14 00:21:11 +0800

    feat：邮件发送

--- src/views/system/email/email_send/hook.tsx
+++ src/views/system/email/email_send/hook.tsx
@@ -1,24 +1,12 @@
-import dayjs from "dayjs";
-import {
-  roleGetOne,
-  roleListPage,
-  roleRemoveOne,
-  roleSaveOne,
-  roleUpdateOne
-} from "@/api/role";
-import {
-  PageResult,
-  RolePageDTO,
-  RoleSaveOneDTO,
-  RoleUpdateOneDTO
-} from "@/api/interface/role";
-import type { PaginationProps } from "@pureadmin/table";
+import { emailSendDTO } from "@/api/interface/email";
+import { emailSend } from "@/api/email";
 import { reactive, ref, computed, onMounted } from "vue";
 import type { FormRules, FormInstance } from "element-plus";
 import { menuListTree } from "@/api/menu";
 import { menuListTreeVO } from "@/api/interface/menu";
+import type { TabsPaneContext } from "element-plus";
 
-export function EmailSend() {
+export function useEmailSend() {
   const dataList = ref([]);
   const loading = ref(true);
   const buttonClass = computed(() => {
@@ -33,17 +21,15 @@ export function EmailSend() {
   const dialogVisible = ref(false as boolean);
   const title = ref("编辑" as string);
   const type = ref<string>("");
-  const ruleForm = ref<RoleUpdateOneDTO>({
-    roleId: null,
-    roleName: "",
-    menuIds: [],
-    description: ""
+  const ruleForm = ref<emailSendDTO>({
+    subject: "",
+    tos: "",
+    content: ""
   });
   const rules = reactive<FormRules>({
-    roleName: [
-      { required: true, message: "请输入角色名称", trigger: "blur" },
-      { min: 1, max: 20, message: "最大输入20个字符", trigger: "blur" }
-    ]
+    subject: [{ required: true, message: "请输入邮件标题", trigger: "blur" }],
+    tos: [{ required: true, message: "请选择收件用户", trigger: "change" }],
+    content: [{ required: true, message: "请输入邮件正文", trigger: "blur" }]
   });
   const menuList = ref<menuListTreeVO[]>([]);
   const treeProps = {
@@ -51,48 +37,7 @@ export function EmailSend() {
     label: "menuName"
   };
 
-  // 新增 / 编辑
-  async function handleUpdate(ty, row) {
-    ruleForm.value = {};
-    if (ty !== "add") {
-      const data = await roleGetOne({ roleId: row.roleId });
-      ruleForm.value = data;
-    }
-    type.value = ty;
-    ty === "add" ? (title.value = "新增") : (title.value = "编辑");
-    dialogVisible.value = true;
-  }
-
-  function handleClose() {
-    dialogVisible.value = false;
-  }
-
-  async function handleDelete(row) {
-    await roleRemoveOne({ roleId: row.roleId });
-    await onSearch();
-  }
-
-  function handleSizeChange(val: number) {
-    console.log(`${val} items per page`);
-  }
-
-  function handleCurrentChange(val: number) {
-    console.log(`current page: ${val}`);
-  }
-
-  function handleSelectionChange(val) {
-    console.log("handleSelectionChange", val);
-  }
-
-  async function onSearch() {
-    loading.value = true;
-    const data: PageResult = await roleListPage(form);
-    dataList.value = data.result;
-    pagination.total = data.total;
-    setTimeout(() => {
-      loading.value = false;
-    }, 500);
-  }
+  const activeName = ref("first");
 
   // 查询菜单树
   async function menuTree() {
@@ -100,44 +45,34 @@ export function EmailSend() {
     menuList.value = data;
   }
 
+  const handleSelectEmail = () => {
+    dialogVisible.value = true;
+  };
+
+  const handleClickTab = (tab: TabsPaneContext, event: Event) => {
+    console.log(tab, event);
+  };
+
+  function handleClose() {
+    dialogVisible.value = false;
+  }
+
   const resetForm = formEl => {
     if (!formEl) return;
     formEl.resetFields();
-    onSearch();
   };
 
   const handleCancel = formEl => {
     if (!formEl) return;
     formEl.resetFields();
     dialogVisible.value = false;
-    onSearch();
   };
 
-  // 调用编辑 / 新增接口
-  const update = async data => {
-    loading.value = true;
-    if (type.value === "add") {
-      const _obj: RoleSaveOneDTO = {
-        roleName: data.roleName,
-        description: data.description
-      };
-      await roleSaveOne(_obj);
-    } else {
-      await roleUpdateOne(data);
-    }
-    setTimeout(() => {
-      loading.value = false;
-      dialogVisible.value = false;
-      onSearch();
-    }, 500);
-  };
-
-  const handleOk = async (formEl: FormInstance | undefined) => {
+  const handleSend = async (formEl: FormInstance | undefined) => {
     if (!formEl) return;
     await formEl.validate(async valid => {
       if (valid) {
-        await update(ruleForm.value);
-        ruleForm.value = {};
+        await emailSend(ruleForm.value);
       } else {
         return;
       }
@@ -145,16 +80,12 @@ export function EmailSend() {
   };
 
   onMounted(() => {
-    onSearch();
     menuTree();
   });
 
   return {
-    form,
     loading,
-    columns,
     dataList,
-    pagination,
     buttonClass,
     dialogVisible,
     title,
@@ -163,15 +94,12 @@ export function EmailSend() {
     type,
     menuList,
     treeProps,
-    onSearch,
+    activeName,
     resetForm,
-    handleUpdate,
-    handleDelete,
-    handleSizeChange,
-    handleCurrentChange,
-    handleSelectionChange,
+    handleSend,
+    handleCancel,
+    handleSelectEmail,
     handleClose,
-    handleOk,
-    handleCancel
+    handleClickTab
   };
 }
--- src/views/system/email/email_send/index.vue
+++ src/views/system/email/email_send/index.vue
@@ -1,79 +1,58 @@
 <script setup lang="ts">
-import {ref, onBeforeUnmount, shallowRef, onMounted} from "vue";
-import {EmailSend} from "./hook";
-import {PureTableBar} from "@/components/RePureTableBar";
-import {useRenderIcon} from "@/components/ReIcon/src/hooks";
-
-import Delete from "@iconify-icons/ep/delete";
-import EditPen from "@iconify-icons/ep/edit-pen";
-import Search from "@iconify-icons/ep/search";
-import Refresh from "@iconify-icons/ep/refresh";
-import AddFill from "@iconify-icons/ri/add-circle-line";
-import '@wangeditor/editor/dist/css/style.css' // 引入 css
-import {Editor, Toolbar} from '@wangeditor/editor-for-vue'
+import { ref, onBeforeUnmount, shallowRef, onMounted } from "vue";
+import { useEmailSend } from "./hook";
+import "@wangeditor/editor/dist/css/style.css"; // 引入 css
+import { Editor, Toolbar } from "@wangeditor/editor-for-vue";
 
 defineOptions({
   name: "EmailSend"
 });
 
-const formRef = ref();
 const ruleFormRef = ref();
 const {
-  form,
-  loading,
-  columns,
-  dataList,
-  pagination,
-  onSearch,
-  resetForm,
-  handleUpdate,
-  handleDelete,
-  handleSizeChange,
-  handleCurrentChange,
-  handleSelectionChange,
-  handleClose,
-  dialogVisible,
-  title,
   ruleForm,
   rules,
-  handleOk,
-  handleCancel,
-  type,
-  menuList,
-  treeProps
+  // activeName,
+  handleSend,
+  // handleCancel,
+  // handleSelectEmail,
+  resetForm
+  // handleClose,
+  // handleClickTab,
+  // dialogVisible
 } = useEmailSend();
 
 // 编辑器实例，必须用 shallowRef
-const editorRef = shallowRef()
+const editorRef = shallowRef();
 
-// 内容 HTML
-const valueHtml = ref('<p>hello</p>')
+// // 内容 HTML
+// const valueHtml = ref("");
 
-// 模拟 ajax 异步获取内容
-onMounted(() => {
-  setTimeout(() => {
-    valueHtml.value = '<p>模拟 Ajax 异步设置内容</p>'
-  }, 1500)
-})
+// // 模拟 ajax 异步获取内容
+// onMounted(() => {
+//   setTimeout(() => {
+//     valueHtml.value = "";
+//   }, 1500);
+// });
 
-const toolbarConfig = {}
-const editorConfig = {placeholder: '请输入内容...'}
+const toolbarConfig = {};
+const editorConfig = { placeholder: "请输入内容..." };
 
 // 组件销毁时，也及时销毁编辑器
 onBeforeUnmount(() => {
-  const editor = editorRef.value
-  if (editor == null) return
-  editor.destroy()
-})
+  const editor = editorRef.value;
+  if (editor == null) return;
+  editor.destroy();
+});
 
-const handleCreated = (editor) => {
-  editorRef.value = editor // 记录 editor 实例，重要！
-}
+const handleCreated = editor => {
+  editorRef.value = editor; // 记录 editor 实例，重要！
+};
 </script>
 
 <template>
   <div class="main">
-    <div>
+    <div class="bg-white">
       <el-form
         ref="ruleFormRef"
         :model="ruleForm"
@@ -83,10 +62,10 @@ const handleCreated = (editor) => {
         status-icon
       >
         <el-form-item label="邮件标题：" prop="subject">
-          <el-input v-model="ruleForm.subject"/>
+          <el-input class="input_width" clearable v-model="ruleForm.subject" />
         </el-form-item>
         <el-form-item label="收件用户：" prop="tos">
-          <el-input v-model="ruleForm.tos"/>
+          <el-input class="input_width" clearable v-model="ruleForm.tos" />
         </el-form-item>
         <el-form-item label="邮件正文：" prop="content">
           <div style="border: 1px solid #ccc">
@@ -94,25 +73,52 @@ const handleCreated = (editor) => {
               style="border-bottom: 1px solid #ccc"
               :editor="editorRef"
               :defaultConfig="toolbarConfig"
-              :mode="mode"
+              mode="default"
             />
             <Editor
-              style="height: 500px; overflow-y: hidden;"
-              v-model="valueHtml"
+              style="height: 500px; overflow-y: hidden"
+              v-model="ruleForm.content"
               :defaultConfig="editorConfig"
-              :mode="mode"
+              mode="default"
               @onCreated="handleCreated"
             />
           </div>
         </el-form-item>
         <el-form-item>
-          <el-button @click="handleCancel(ruleFormRef)">取消</el-button>
-          <el-button type="primary" @click="handleOk(ruleFormRef)">
-            确定
+          <el-button @click="resetForm(ruleFormRef)">内容重置</el-button>
+          <el-button type="primary" @click="handleSend(ruleFormRef)">
+            发送邮件
           </el-button>
         </el-form-item>
       </el-form>
     </div>
+    <!-- 选择用户邮箱弹框 -->
+    <!-- <el-dialog
+      v-model="dialogVisible"
+      title="选择收件人"
+      width="40%"
+      :before-close="handleClose"
+    >
+      <div>
+        <el-tabs
+          v-model="activeName"
+          class="demo-tabs"
+          @tab-click="handleClickTab"
+        >
+          <el-tab-pane label="用户" name="first">用户</el-tab-pane>
+          <el-tab-pane label="部门" name="second">部门</el-tab-pane>
+          <el-tab-pane label="角色" name="third">角色</el-tab-pane>
+        </el-tabs>
+      </div>
+      <template #footer>
+        <span class="dialog-footer">
+          <el-button @click="dialogVisible = false">取消</el-button>
+          <el-button type="primary" @click="dialogVisible = false">
+            确定
+          </el-button>
+        </span>
+      </template>
+    </el-dialog> -->
   </div>
 </template>
 
@@ -121,7 +127,11 @@ const handleCreated = (editor) => {
   margin: 0;
 }
 
-.demo-ruleForm {
-  width: 50%;
+.bg-white {
+  background-color: #fff;
+  padding: 50px 100px;
+  .input_width {
+    width: 50%;
+  }
 }
 </style>
