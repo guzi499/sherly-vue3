26327eb87be2e1f84b04e20e15974fea4f3821bd
陈雪丽 <1193832236@qq.com>
2023-02-13 15:12:45 +0800

    feat：邮箱配置

---
+++ src/views/system/email/email_config/hook.tsx
@@ -0,0 +1,74 @@
+import {reactive, ref, computed, onMounted} from "vue";
+import type {FormRules, FormInstance} from "element-plus";
+import {emailGetOneVO, emailSaveOrUpdateDTO} from "@/api/interface/email";
+import {emailGetOne, emailSaveOrUpdate} from "@/api/email";
+
+export function useEmailConfig() {
+  const loading = ref(true);
+  const ruleForm = ref<emailSaveOrUpdateDTO>({
+    senderEmail: "",
+    senderUser: "",
+    password: "",
+    host: "",
+    port: ""
+  });
+  const rules = reactive<FormRules>({
+    senderEmail: [
+      {required: true, message: "请输入发件人邮箱", trigger: "blur"},
+    ],
+    senderUser: [
+      {required: true, message: "请输入发件人名称", trigger: "blur"},
+    ],
+    password: [
+      {required: true, message: "请输入邮箱密码", trigger: "blur"},
+    ],
+    host: [
+      {required: true, message: "请输入SMTP地址", trigger: "blur"},
+    ],
+    port: [
+      {required: true, message: "请输入SMTP端口", trigger: "blur"},
+    ]
+  });
+  async function onSearch() {
+    loading.value = true;
+    const data: emailGetOneVO = await emailGetOne();
+    ruleForm.value = data
+    setTimeout(() => {
+      loading.value = false;
+    }, 500);
+  }
+
+  const handleCancel = formEl => {
+    if (!formEl) return;
+    formEl.resetFields();
+    onSearch();
+  };
+
+  const handleOk = async (formEl: FormInstance | undefined) => {
+    if (!formEl) return;
+    await formEl.validate(async valid => {
+      if (valid) {
+        loading.value = true
+        const data: emailGetOneVO = await emailSaveOrUpdate(ruleForm.value);
+        setTimeout(() => {
+          loading.value = false;
+        }, 500);
+      } else {
+        return;
+      }
+    });
+  };
+
+  onMounted(() => {
+    onSearch();
+  });
+
+  return {
+    loading,
+    ruleForm,
+    rules,
+    onSearch,
+    handleOk,
+    handleCancel
+  };
+}
---
+++ src/views/system/email/email_config/index.vue
@@ -0,0 +1,70 @@
+<script setup lang="ts">
+import {ref} from "vue";
+import {useEmailConfig} from "./hook";
+
+defineOptions({
+  name: "EmailConfig"
+});
+
+const ruleFormRef = ref();
+const {
+  loading,
+  onSearch,
+  ruleForm,
+  rules,
+  handleOk,
+  handleCancel
+} = useEmailConfig();
+</script>
+
+<template>
+  <div class="main">
+    <div class="bg-white">
+      <el-form
+        ref="ruleFormRef"
+        :model="ruleForm"
+        :rules="rules"
+        label-width="120px"
+        class="demo-ruleForm"
+        status-icon
+      >
+        <el-form-item label="发件人邮箱" prop="senderEmail">
+          <el-input clearable v-model="ruleForm.senderEmail"/>
+        </el-form-item>
+        <el-form-item label="发件人名称" prop="senderUser">
+          <el-input clearable v-model="ruleForm.senderUser"/>
+        </el-form-item>
+        <el-form-item label="邮箱密码" prop="password">
+          <el-input clearable v-model="ruleForm.password"/>
+        </el-form-item>
+        <el-form-item label="SMTP地址" prop="host">
+          <el-input clearable v-model="ruleForm.host"/>
+        </el-form-item>
+        <el-form-item label="SMTP端口" prop="port">
+          <el-input clearable v-model="ruleForm.port"/>
+        </el-form-item>
+        <el-form-item>
+          <el-button @click="handleCancel(ruleFormRef)">一键还原</el-button>
+          <el-button type="primary" @click="handleOk(ruleFormRef)">
+            更新配置
+          </el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+:deep(.el-dropdown-menu__item i) {
+  margin: 0;
+}
+
+.bg-white {
+  background-color: #fff;
+  padding: 50px 100px;
+
+  .demo-ruleForm {
+    width: 50%;
+  }
+}
+</style>
